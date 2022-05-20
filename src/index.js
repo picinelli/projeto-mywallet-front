f5119df269d5ec80f8817db90d3e0b28c9bdf9b9
pedro imbrech <elprogramadorpedro@gmail.com>
2022-05-20 09:15:55 -0300

    rev

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './Components/App'
-
-import './assets/css/reset.css'
-import './assets/css/style.css'
-
-ReactDOM.render(<App />, document.querySelector(".root"));
